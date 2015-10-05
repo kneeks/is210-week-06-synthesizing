4a6ccbf490c088c400e879f8f93915c99e4b9dc2
Vagrant User <mr.nikovaccaro@gmail.com>
2015-10-05 03:19:44 +0000

    updated docstring

--- task_02.py
+++ task_02.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module counts total amount of guests and total tables needed."""
+"""This module prepares an email template."""
 
 
 def prepare_email(appointments):
-    appointments = [{}]
-    email = ('Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe')
+    """
+    This function sends automatic email responses with designated format.
+
+    Args:
+        appointments (mixed): name, date
+
+    Returns:
+        mixed: automatic response with appointments filled in designated areas
+
+    Examples:
+
+        >>> prepare_email([('Jen', '2015'), ('Max', 'March 3')]
+        ['Dear Jen,\nI look forward to meeting with you on 2015.\nBest,\nMe',
+        'Dear Max,\nI look forward to meeting you on March 3.\nBest\nMe']
+
+    """
+    email = ('Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe'.format(appointments)
     for names in appointments:
         email.format(appointments)
         return email
